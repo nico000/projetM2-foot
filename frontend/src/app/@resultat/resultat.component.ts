fd81b1f3edd9f4796a7fe7cd6a86ab083d17bdbd
maxence59 <roussel59330@gmail.com>
2024-01-22 18:18:24 +0100

    home

---
+++ frontend/src/app/@resultat/resultat.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'navigation-bar',
+    templateUrl: './resultat.component.html',
+    styleUrls: ['./resultat.component.css' ]
+})
+
+/** Composant qui represente le header du site */
+export class ResultatComponent {
+
+    constructor() {}
+}

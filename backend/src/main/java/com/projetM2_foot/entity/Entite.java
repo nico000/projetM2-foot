2c7caf4f6cfa4ef07d1d83949481d7a596260747
Nicolas Laniau <nicolas.laniau@gmail.com>
2024-01-23 11:13:31 +0100

    Modif entity Entite

--- backend/src/main/java/com/projetM2_foot/entity/Entite.java
+++ backend/src/main/java/com/projetM2_foot/entity/Entite.java
@@ -1,16 +1,20 @@
 package com.projetM2_foot.entity;
 
 import lombok.*;
-import org.springframework.data.annotation.Id;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.session.delegates.GraphIdDelegate;
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@NodeEntity
 public class Entite {
+
     @Id
-    private String id; // Type temporaire voir pour changer le type
+    private int id; // Type temporaire voir pour changer le type
     private int numero; // le ballon porte le numéro 0 le reste des numéro lambda
     private int position; // position = numero de zone ?
 }

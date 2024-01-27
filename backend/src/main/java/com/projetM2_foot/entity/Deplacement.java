e492c206b66e818530f8618cb379a96cf8e28eae
Nicolas Laniau <nicolas.laniau@gmail.com>
2024-01-28 00:55:52 +0100

    Modification entité Deplacement

--- backend/src/main/java/com/projetM2_foot/entity/Deplacement.java
+++ backend/src/main/java/com/projetM2_foot/entity/Deplacement.java
@@ -1,7 +1,11 @@
 package com.projetM2_foot.entity;
 
 import lombok.*;
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
+
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -11,10 +15,29 @@ import org.neo4j.ogm.annotation.NodeEntity;
 @NodeEntity
 public class Deplacement {
 
+    @Id
+    @GeneratedValue
+    private Long id;
+
     /** numero de l'action */
-    private int action;
-    private Entite entite;
-    private int position_debut;
-    private int position_fin;
+    @NotNull
+    private int numAction;
+    /** numero de la scene */
+    @NotNull
+    private int numScene;
+    /** numero du bloc */
+    @NotNull
+    private int numBloc;
+
+    /** [FK] Entité */
+    private Long idEntite;
+    /** Position X de début */
+    private int startPosX;
+    /** Position Y de début */
+    private int startPosY;
+    /** Position X de fin */
+    private int endPosX;
+    /** Position Y de fin */
+    private int endPosY;
 
 }

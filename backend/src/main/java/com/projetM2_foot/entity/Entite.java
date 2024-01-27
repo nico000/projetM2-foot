3fe6890871d496fa40ff31fa59d35984cc22839e
Nicolas Laniau <nicolas.laniau@gmail.com>
2024-01-27 17:11:32 +0100

    Modification entité Entite

--- backend/src/main/java/com/projetM2_foot/entity/Entite.java
+++ backend/src/main/java/com/projetM2_foot/entity/Entite.java
@@ -1,7 +1,11 @@
 package com.projetM2_foot.entity;
 
 import lombok.*;
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
+
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -10,6 +14,28 @@ import org.neo4j.ogm.annotation.NodeEntity;
 @NoArgsConstructor
 @NodeEntity
 public class Entite {
-    private int numero; // le ballon porte le numéro 0 le reste des numéro lambda
-    private int position; // position = numero de zone ?
+
+    @Id @GeneratedValue
+    private Long id;
+
+    /** Type d'entité :
+     * 0 -> ballon /
+     * 1 -> joueur /
+     * 2 -> objet
+     */
+    @NotNull
+    private int type;
+
+    /** Numero que porte un joueur (par défaut 0 si entité n'est pas un joueur) */
+    @NotNull
+    private int numero = 0;
+
+    /** Position initial coordonnées X */
+    @NotNull
+    private int initialPosX;
+
+    /** Position initial coordonnées Y */
+    @NotNull
+    private int initialPosY;
+
 }

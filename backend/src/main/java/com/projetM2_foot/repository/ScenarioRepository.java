49990fa10ad59f1a393fc070e4f65e99c9207832
Nicolas Laniau <nicolas.laniau@gmail.com>
2024-01-29 14:30:12 +0100

    commit repository

--- backend/src/main/java/com/projetM2_foot/repository/ScenarioRepository.java
+++ backend/src/main/java/com/projetM2_foot/repository/ScenarioRepository.java
@@ -4,7 +4,11 @@ import com.projetM2_foot.entity.Scenario;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ScenarioRepository extends CrudRepository<Scenario , Long> {
 
+    @Override
+    List<Scenario> findAll();
 }

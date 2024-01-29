b22d12812ab1c0958bbeafa169283b96bcda71b8
Nicolas Laniau <nicolas.laniau@gmail.com>
2024-01-29 14:15:40 +0100

    commit request

--- backend/src/main/java/com/projetM2_foot/api/request/ScenarioRequestCreate.java
+++ backend/src/main/java/com/projetM2_foot/api/request/ScenarioRequestCreate.java
@@ -6,17 +6,15 @@ import lombok.*;
 @Builder
 public class ScenarioRequestCreate {
 
-    private String name;
+    private String nom;
+    private String mode_scene;
+    private String terrain_couleur;
+    private String terrain_taille;
+    private int joueur_nb;
 
-    private Boolean threeDim;
-    private String modeScene;
-    private String groundColor;
-    private int groundWidth;
-
-    private int playerNb;
-    private int playerIcon;
-
-    private Boolean displayArea;
-    private int colonArea;
-    private int corridorArea;
+    private Boolean zone_use;
+    private Boolean zone_display;
+    private int zone_nb_zone;
+    private int zone_nb_couloir;
+    private int zone_epaisseur;
 }

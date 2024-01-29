0504c5a7a2097dcc372eb41c1dc243e3a032817c
Nicolas Laniau <nicolas.laniau@gmail.com>
2024-01-29 14:16:25 +0100

    commit response

---
+++ backend/src/main/java/com/projetM2_foot/api/response/EntiteResponse.java
@@ -0,0 +1,16 @@
+package com.projetM2_foot.api.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class EntiteResponse {
+
+    private Long id;
+    private Long scenario;
+    private int type;
+    private int numero;
+    private int x;
+    private int y;
+}

7dda44edb2cc76b32f453fd99be9c0f802c00d62
Nicolas Laniau <nicolas.laniau@gmail.com>
2024-02-01 11:58:02 +0100

    Update type position (int -> float)

--- backend/src/main/java/com/projetM2_foot/api/request/EntiteRequestCreate.java
+++ backend/src/main/java/com/projetM2_foot/api/request/EntiteRequestCreate.java
@@ -1,13 +1,8 @@
 package com.projetM2_foot.api.request;
 
-import com.projetM2_foot.entity.Scenario;
+
 import lombok.Builder;
 import lombok.Data;
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
 
 @Data
 @Builder

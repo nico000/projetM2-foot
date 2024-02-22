a32fdc4669584528e7aefd45da153e888f205eb4
Nicolas Laniau <nicolas.laniau@gmail.com>
2024-02-22 12:47:30 +0100

    modif request create experience

--- backend/src/main/java/com/projetM2_foot/api/request/ScenarioRequestCreate.java
+++ backend/src/main/java/com/projetM2_foot/api/request/ScenarioRequestCreate.java
@@ -11,11 +11,11 @@ public class ScenarioRequestCreate {
     @NotNull
     private String nom;
 
+    private String mode_scene;
     private String terrain_couleur;
     private String terrain_taille;
     private int joueur_nb;
 
-    private String mode_scene;
     private Boolean zone_use;
     private Boolean zone_display;
     private int zone_nb_zone;
